212c9595774bac1472d844cfee201a08618cc722
pvig <patricevigouroux@yahoo.fr>
2024-03-14 07:48:09 +0100

    first test

--- backend/boutique-django/boutique/tests.py
+++ backend/boutique-django/boutique/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from boutique.models import Client
 
-# Create your tests here.
+class AccountTests(TestCase):
+    def setUp(self):
+        Client.objects.create(username="user1", password="roar", email="", nom="")
+
+    def test_account_can_be_created(self):
+        """Account can be created"""
+        user1 = Client.objects.get(username="user1")
+        self.assertEqual(user1.username, 'user1')
