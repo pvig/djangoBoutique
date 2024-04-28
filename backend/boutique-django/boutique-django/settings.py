8a10e5642049ef463b4958738a2d8b929b778326
pvig <patricevigouroux@yahoo.fr>
2024-04-28 13:55:00 +0200

    docker pour backend

--- backend/boutique-django/boutique-django/settings.py
+++ backend/boutique-django/boutique-django/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 ]
 
 ROOT_URLCONF = 'boutique-django.urls'
