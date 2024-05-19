ba09aa9e3e242ed87939acbd0f738e7c58394096
pvig <patricevigouroux@yahoo.fr>
2024-05-19 23:08:49 +0200

    boutique models in admin

--- backend/boutique_django/core_apps/boutique/admin.py
+++ backend/boutique_django/core_apps/boutique/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+from .models import Produit
+from .models import Vente
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display=(
+    'username', 'email',
+)
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display=(
+    'nom', 'prixHT', 'poids', 'reference',
+)
+
+class VenteAdmin(admin.ModelAdmin):
+    list_display=(
+    'client', 'dateVente', 'prixProduitsHT', 'prixProduitsTTC',
+)
+
+
+admin.site.register(Client,ClientAdmin)
+admin.site.register(Produit,ProduitAdmin)
+admin.site.register(Vente,VenteAdmin)
